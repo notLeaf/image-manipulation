2ebd6f3b8c25eb2e222d62562640ebab06f5d085
notLeaf <70775569+notLeaf@users.noreply.github.com>
2021-12-20 19:09:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const client = new Client({
 });
 module.exports = client;
 
-// Global Variables
+
 client.commands = new Collection();
 client.slashCommands = new Collection();
 client.config = require("./config.json");
 
-// Initializing the project
 require("./handler")(client);
 
 client.login(client.config.token);
