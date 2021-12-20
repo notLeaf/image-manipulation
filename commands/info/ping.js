6384c2a1047a7d344762d363f47cc6d9608cdfde
notLeaf <70775569+notLeaf@users.noreply.github.com>
2021-12-20 19:12:13 +0100

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -3,12 +3,7 @@ const { Message, Client } = require("discord.js");
 module.exports = {
     name: "ping",
     aliases: ['p'],
-    /**
-     *
-     * @param {Client} client
-     * @param {Message} message
-     * @param {String[]} args
-     */
+
     run: async (client, message, args) => {
         message.channel.send(`${client.ws.ping} ws`);
     },
